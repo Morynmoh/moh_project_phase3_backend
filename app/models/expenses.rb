c5d379769584d03b6c666b66f29d8338dfe93b4f
Maureen <morynmoh@gmail.com>
2023-07-27 03:31:05 +0300

    pg gem

--- app/models/expenses.rb
+++ app/models/expenses.rb
@@ -2,8 +2,7 @@ class Expense < ActiveRecord::Base
     belongs_to :user 
     belongs_to :category 
     validates :description, :date, :amount, :category_id, presence: true
-    
-  
+      
 end
 
 
